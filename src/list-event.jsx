ee370433ee42ea5329bb1ac32fd52b96989cbc14
Lewis Healey <lewis.healey@firebox.com>
2016-01-03 17:09:53 +0000

    Event list component

---
+++ src/list-event.jsx
@@ -0,0 +1,169 @@
+var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://boiling-fire-2669.firebaseio.com/';
+var ReactFire = require('reactfire');
+
+
+module.exports = React.createClass({
+  mixins: [ReactFire],
+  getInitialState: function() {
+    return {  
+      event: this.props.event,
+      userId: this.props.userId,
+      guestId: this.props.guestId,
+      eventId: this.props.id,
+      attending: false,
+      meals: false
+    }
+  },
+  componentWillMount: function() {
+      var mealRef = new Firebase(rootUrl + 'users/' + this.state.userId + "/meals/");
+      this.bindAsObject(mealRef, 'meals');
+  },
+  render: function() {
+    console.log(this.state.meals);
+  return <div className="view__event">
+
+    <div className="column__double"> 
+      <div className="column--nest"> 
+
+        <h4>{this.state.event.name}</h4>
+
+        {this.state.event.meals &&
+          <h6>Has meals</h6>
+
+        }
+
+        <a className="btn btn-success" onClick={this.handleAttending.bind(this,true)}>Attending</a> 
+        <a className="btn btn-danger" onClick={this.handleAttending.bind(this,false)}>Cannot Attend</a>
+
+        {(this.state.attending || this.state.event.guests[this.state.guestId].attending == true)&& 
+
+          <div>
+            <h4>Attending</h4>
+
+            {this.state.meals &&
+
+              <div>
+
+                <select className="form-control" ref="mealChoice">
+                  <option>Select a meal</option>
+              
+                    {Object.keys(this.state.event.meals).map(function (key, i) {
+                      return <option key={i} value={key}>{this.state.meals[key].name}</option>
+                    }.bind(this))}
+
+                </select>
+
+                <a onClick={this.handleMeals}>Save</a>
+
+              </div>
+
+            }
+
+
+          </div>
+
+        }
+
+      </div>
+    </div>
+
+    <div className="column__half column--img"> 
+      <img src={"http://maps.googleapis.com/maps/api/staticmap?center=" + this.state.event.postcode + "&zoom=16&size=500x500&markers=" + this.state.event.postcode + "&sensor=false"} />
+    </div>
+
+  </div>
+   
+  },
+  removeMeal: function(mealId) {
+    var guestRef = new Firebase(rootUrl + 'users/' + this.state.userId + '/guests/' + this.state.guestId + "/meals");
+    var mealRef = new Firebase(rootUrl + 'users/' + this.state.userId + '/meals/' + mealId + "/guests/" + this.state.guestId);
+    guestRef.remove();
+    mealRef.remove();
+  },
+  handleMeals: function() {
+    var timeInMs = Date.now();
+    var mealId = this.refs.mealChoice.getDOMNode().value ;
+    var guestRef = new Firebase(rootUrl + 'users/' + this.state.userId + '/guests/' + this.state.guestId);
+    var mealRef = new Firebase(rootUrl + 'users/' + this.state.userId + '/meals/' + mealId);
+    console.log(event.target.value);
+
+    guestRef.child("meals").update({
+      [this.state.eventId]: mealId
+      }, function(error) {
+        // Error report event
+        if (error) {
+        console.log("Event could not be saved" + error);
+      } else {
+        console.log("Meal added to guest");
+      }
+
+    });
+
+    mealRef.child("guests").update({
+      [this.state.guestId]: true
+      }, function(error) {
+        // Error report event
+        if (error) {
+        console.log("Event could not be saved" + error);
+      } else {
+        console.log("Meal added to meals");
+      }
+
+    });
+
+  },
+  handleAttending: function(truth) {
+    var guestRef = new Firebase(rootUrl + 'users/' + this.state.userId + '/guests/' + this.state.guestId);
+    var eventRef = new Firebase(rootUrl + 'users/' + this.state.userId + '/events/' + this.state.eventId + '/guests/' + this.state.guestId);
+
+    // console.log(this.state.userId);
+    // console.log(this.state.guestId);
+    // console.log(this.state.eventId);
+
+    var timeInMs = Date.now();
+
+    // If true then flip state
+    if(truth == true) {
+
+      this.setState({
+        attending: true
+      });
+
+    } else {
+
+      this.setState({
+        attending: false
+      });
+
+    }
+
+    // Set attending event key to the guest
+      guestRef.child("attending").update({
+          [this.state.eventId]: truth
+        }, function(error) { if (error) { 
+          console.log("Could not set attending to guest " + error);
+        } else {
+          console.log("Set " + truth + " attending to guest");
+        }
+
+    // Update the event to save guest as attending
+    eventRef.update({ 
+        attending: truth
+      }, function(error) {
+        // Error report event
+        if (error) {
+        console.log("Nope to update event" + error);
+      } else {
+        console.log("Updated event" + truth);
+      }
+
+      });
+
+    });
+
+
+  }
+
+});
--- src/view.jsx
+++ src/view.jsx
@@ -3,6 +3,7 @@ var ReactFire = require('reactfire');
 var Firebase = require('firebase');
 var rootUrl = 'https://boiling-fire-2669.firebaseio.com/';
 
+var ListEvent = require('./list-event');
 
 module.exports = React.createClass({
   mixins: [ReactFire],
@@ -16,12 +17,10 @@ module.exports = React.createClass({
 
     var firebaseRef = new Firebase(rootUrl + 'users/' + this.props.params.userId + '/guests/' + this.props.params.guestId);
     var eventRef = new Firebase(rootUrl + 'users/' + this.props.params.userId + "/events/");
-    var mealRef = new Firebase(rootUrl + 'users/' + this.props.params.userId + "/meals/");
 
     // Bind Events, Meals & Guests to states
     this.bindAsObject(eventRef, 'events'); 
     this.bindAsObject(firebaseRef, 'guest');
-    this.bindAsObject(mealRef, 'meals');
 
   },
   componentDidMount: function() {
@@ -36,76 +35,17 @@ module.exports = React.createClass({
     // If component is loaded
     if(this.state.loaded && this.state.guest.events) {
 
-    var content = Object.keys(this.state.guest.events).map(function (key, i) {
+      var content = Object.keys(this.state.guest.events).map(function (key, i) {
 
-              return <div key={i} className="view__event"> 
+        return <ListEvent event={this.state.events[key]} key={i} id={key} i={i} userId={this.props.params.userId} guestId={this.props.params.guestId} />
                 
-                <div className="column__double"> 
-                  <div className="column--nest"> 
+      }.bind(this));
 
-                    <h4>{this.state.events[key].name}</h4>
-                    <p>{this.state.events[key].address} {this.state.events[key].postcode}</p>
+    } else {
 
-                    {( ! this.state.guest.meals && this.state.events[key].guests[this.props.params.guestId].attending == true) && 
-                      <div>
+      var content = "Loading";
 
-                        <div>
-
-                        <select className="form-control" ref="mealChoice">
-                          <option>Select a meal</option>
-                        
-                          {Object.keys(this.state.events[key].meals).map(function (mKey, i) {
-                            return <option key={i} value={mKey}>{this.state.meals[mKey].name}</option>
-                          }.bind(this))}
-
-                        </select>
-
-                        <a onClick={this.handleMeals}>Save</a>
-
-                        </div>
-
-
-
-
-                      </div>
-
-                    }
-
-                    {(this.state.events[key].guests[this.props.params.guestId].attending == true) &&
-                      <div>
-                      <h4>Attending</h4>
-
-                      {Object.keys(this.state.guest.events)[i] &&
-
-                      <div>
-                        <h4>{this.state.meals[Object.keys(this.state.guest.meals)[0]].name}</h4>
-                        <a className={this.removeMeal.bind(this,Object.keys(this.state.guest.meals)[0])}>Remove</a>
-                      </div>
-
-                      } 
-
-                      </div>
-
-                    }
-
-                    <a className="btn btn-success" onClick={this.handleAttending.bind(this,key,this.props.params.guestId,this.state.userId,true)}>Attending</a> 
-                    <a className="btn btn-danger" onClick={this.handleAttending.bind(this,key,this.props.params.guestId,this.state.userId,false)}>Cannot Attend</a>
-
-                </div>
-
-              </div>
-
-              <div className="column__half column--img"> 
-                <img src={"http://maps.googleapis.com/maps/api/staticmap?center=" + this.state.events[key].postcode + "&zoom=16&size=500x500&markers=" + this.state.events[key].postcode + "&sensor=false"} />
-              </div>
-
-              </div>
-
-            }.bind(this));
-
-  } else {
-    var content = "Loading";
-  }
+    }
 
   
   return <div className="view">
@@ -127,81 +67,6 @@ module.exports = React.createClass({
       </div>
 
     </div>
-
-  },
-  removeMeal: function(mealId) {
-    var guestRef = new Firebase(rootUrl + 'users/' + this.props.params.userId + '/guests/' + this.props.params.guestId + "/meals");
-    var mealRef = new Firebase(rootUrl + 'users/' + this.props.params.userId + '/meals/' + mealId + "/guests/" + this.props.params.guestId);
-    guestRef.remove();
-    mealRef.remove();
-  },
-  handleMeals: function() {
-    var timeInMs = Date.now();
-    var mealId = this.refs.mealChoice.getDOMNode().value ;
-    var guestRef = new Firebase(rootUrl + 'users/' + this.props.params.userId + '/guests/' + this.props.params.guestId);
-    var mealRef = new Firebase(rootUrl + 'users/' + this.props.params.userId + '/meals/' + mealId);
-    console.log(event.target.value);
-
-    guestRef.child("meals").set({
-      [mealId]: true
-      }, function(error) {
-        // Error report event
-        if (error) {
-        console.log("Event could not be saved" + error);
-      } else {
-        console.log("Meal added to guest");
-      }
-
-    });
-
-    mealRef.child("guests").set({
-      [this.props.params.guestId]: true
-      }, function(error) {
-        // Error report event
-        if (error) {
-        console.log("Event could not be saved" + error);
-      } else {
-        console.log("Meal added to meals");
-      }
-
-    });
-
-  },
-  handleAttending: function(eventId, guestId, userId, truth) {
-    console.log(eventId);
-    console.log(userId);
-    console.log(guestId);
-
-    var timeInMs = Date.now();
-    var guestRef = new Firebase(rootUrl + 'users/' + userId + '/guests/' + guestId);
-    var eventRef = new Firebase(rootUrl + 'users/' + userId + '/events/' + eventId + '/guests/' + guestId);
-
-    eventRef.update({ 
-      attending: truth
-    }, function(error) {
-      // Error report event
-      if (error) {
-      console.log("Nope to update event" + error);
-    } else {
-      console.log("Updated event" + truth);
-    }
-
-    });
-
-    guestRef.child("attending").set({
-      [eventId]: truth
-      }, function(error) {
-        // Error report event
-        if (error) {
-        console.log("Attending update error " + error);
-      } else {
-        console.log("Guest has been updated with " + truth);
-      }
-
-    });
-
-  },
-  handleNotAttending: function() {
 
   }
 
