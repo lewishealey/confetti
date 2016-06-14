1d194da22cacc898e6e2b08b815a27dd426f741b
Lewis Healey <hello@lewishealey.co.uk>
2016-06-14 23:20:09 +0100

    Mail sender

--- server/mail/notification_attending.php
+++ server/mail/notification_attending.php
@@ -42,17 +42,17 @@ foreach($user->attending as $key => $guest) {
       $events_table;
 
 
-      // $data = array(
-      //           "id" => 1,
-      //             "to" => "hello@lewi.sh",
-      //             "attr" => array(
-      //               "FNAME"=>$user->guests->$key->fname,
-      //               "LNAME"=>$user->guests->$key->lname,
-      //               "EVENT_LOOP"=> "<h1>Hello</h1>"
-      //             )
-      // );
-      //
-      // var_dump($mailin->send_transactional_template($data));
+      $data = array(
+                "id" => 1,
+                  "to" => "hello@lewi.sh",
+                  "attr" => array(
+                    "FNAME"=>$user->guests->$key->fname,
+                    "LNAME"=>$user->guests->$key->lname,
+                    "EVENT_LOOP"=> "<h1>Hello</h1>"
+                  )
+      );
+
+      var_dump($mailin->send_transactional_template($data));
 
 
 
