9a408f785b049f276d9e51b4d1c67335278c67b8
Lewis Healey <lewis.healey@firebox.com>
2016-06-20 17:15:45 +0100

    Composer update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitddb9010af49db97db5c82e414a85b626::getLoader();
+return ComposerAutoloaderInit0c00bb64306363d8764738a77f531d1f::getLoader();
