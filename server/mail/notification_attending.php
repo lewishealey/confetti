09e6c30f517e6c15e5d1d6803f1716a429311e3c
Lewis Healey <hello@lewishealey.co.uk>
2016-06-21 22:31:45 +0100

    Loop through users and send out notifications

--- server/mail/notification_attending.php
+++ server/mail/notification_attending.php
@@ -12,152 +12,168 @@ $fb = Firebase::initialize("https://boiling-fire-2669.firebaseio.com/", "P5Ofkmp
 
 $userData = $fb->get("users/09a57e9a-6d0d-4587-8e2e-7867111a41a2");
 
-// print_r($userData);
+$users = $fb->get("users");
 
 $before = strtotime("-10 minutes");
-
 $mailin = new Mailin('https://api.sendinblue.com/v2.0','RAKxVhObvYnN318W');
 
-foreach($userData["guests"] as $key => $guest) {
 
-  // If has attending or not attending
-  $hasAttending = isset($userData["attending"][$key]);
-  $hasNotAttending = isset($userData["notattending"][$key]);
+foreach($users as $user_id => $userData) {
 
+  if (is_array($userData["guests"])) {
 
-  if($hasAttending || $hasNotAttending) {
+    echo $user_id;
 
-    if($hasAttending) {
-      $a_incron =  $userData["attending"][$key]["date_created"] / 1000 > $before;
-    }
+    foreach($userData["guests"] as $key => $guest) {
 
-    if($hasNotAttending) {
-      $na_incron =  $userData["notattending"][$key]["date_created"] / 1000 > $before;
-    }
+      // If has attending or not attending
+      $hasAttending = isset($userData["attending"][$key]);
+      $hasNotAttending = isset($userData["notattending"][$key]);
+      $a_incron = "";
+      $na_incron = "";
 
 
-    //If attending or not attending is in cron
-    if($a_incron || $na_incron) {
-      $subject = $userData["guests"][$key]["fname"] . " " .$userData["guests"][$key]["lname"] . "";
+      if($hasAttending || $hasNotAttending) {
 
-      $events_table = "<table width='100%' cellspacing='0' cellpadding='0'>";
+        if($hasAttending) {
+          $a_incron =  $userData["attending"][$key]["date_created"] / 1000 > $before;
+        }
 
-        $hasAttendingEvents = isset($userData["attending"][$key]["events"]);
-        $hasNotAttendingEvents = isset($userData["notattending"][$key]["events"]);
+        if($hasNotAttending) {
+          $na_incron =  $userData["notattending"][$key]["date_created"] / 1000 > $before;
+        }
 
-          // echo date('F j, Y, g:i a', $before);
 
-          if($hasAttendingEvents) {
-            // echo date('F j, Y, g:i a', ($userData["attending"][$key]["date_created"] / 1000));
+        //If attending or not attending is in cron
+        if($a_incron || $na_incron) {
+          $subject = $userData["guests"][$key]["fname"] . " " .$userData["guests"][$key]["lname"] . "";
 
-            $subject .= " is attending ";
+          $events_table = "<table width='100%' cellspacing='0' cellpadding='0'>";
 
-            foreach($userData["attending"][$key]["events"] as $id => $event) {
-              $subject.=  $userData["events"][$id]["name"] . ", ";
+            $hasAttendingEvents = isset($userData["attending"][$key]["events"]);
+            $hasNotAttendingEvents = isset($userData["notattending"][$key]["events"]);
 
-              $events_table .= '<tr cellspacing="0" cellpadding="0">
-                <td cellspacing="0" cellpadding="0" style="border-bottom: 2px solid #ECEFF1; text-align: left; padding: 20px 20px 20px 20px; font-size: 16px; color: #8B9299;">
-                  <img src="https://firebasestorage.googleapis.com/v0/b/boiling-fire-2669.appspot.com/o/tick.png?alt=media&token=e41d9b12-d2a8-475b-8974-c86205c20e42" height="16" style="height: 12px; padding-right: 20px;"> ' . $userData["events"][$id]["name"] . '
-                </td>';
+              // echo date('F j, Y, g:i a', $before);
 
-                $eventHasMeals = isset($userData["attending"][$key]["events"][$id]["courses"]);
+              if($hasAttendingEvents) {
+                // echo date('F j, Y, g:i a', ($userData["attending"][$key]["date_created"] / 1000));
 
-                $events_table .= '<td cellspacing="0" cellpadding="0" style="border-bottom: 2px solid #ECEFF1; text-align: left; padding: 20px 20px 20px 20px; font-size: 16px; color: #8B9299;">';
+                $subject .= " is attending ";
 
-                if($eventHasMeals) {
+                foreach($userData["attending"][$key]["events"] as $id => $event) {
+                  $subject.=  $userData["events"][$id]["name"] . ", ";
 
-                    foreach($userData["attending"][$key]["events"][$id]["courses"] as $course_id => $meal) {
-                      $meal_id = $meal["meal_name"];
-                      $events_table .= $userData["courses"][$id][$course_id]["meals"][$meal_id]["name"] . " - ";
+                  $events_table .= '<tr cellspacing="0" cellpadding="0">
+                    <td cellspacing="0" cellpadding="0" style="border-bottom: 2px solid #ECEFF1; text-align: left; padding: 20px 20px 20px 20px; font-size: 16px; color: #8B9299;">
+                      <img src="https://firebasestorage.googleapis.com/v0/b/boiling-fire-2669.appspot.com/o/tick.png?alt=media&token=e41d9b12-d2a8-475b-8974-c86205c20e42" height="16" style="height: 12px; padding-right: 20px;"> ' . $userData["events"][$id]["name"] . '
+                    </td>';
+
+                    $eventHasMeals = isset($userData["attending"][$key]["events"][$id]["courses"]);
+
+                    $events_table .= '<td cellspacing="0" cellpadding="0" style="border-bottom: 2px solid #ECEFF1; text-align: left; padding: 20px 20px 20px 20px; font-size: 16px; color: #8B9299;">';
+
+                    if($eventHasMeals) {
+
+                        foreach($userData["attending"][$key]["events"][$id]["courses"] as $course_id => $meal) {
+                          $meal_id = $meal["meal_name"];
+                          $events_table .= $userData["courses"][$id][$course_id]["meals"][$meal_id]["name"] . " - ";
+                        }
+
                     }
 
+
+                    $events_table .= '</td>';
+                    $events_table.= "</tr>";
+
+
+                    // var_dump($userData["attending"]);
+
+                  }
+
+              }
+
+              if($hasNotAttendingEvents) {
+                echo date('F j, Y, g:i a', ($userData["notattending"][$key]["date_created"] / 1000));
+
+                if($hasAttendingEvents) {
+                  $subject .= " and not ";
+                } else {
+                  $subject .= " is not attending ";
                 }
 
+                foreach($userData["notattending"][$key]["events"] as $id => $event) {
+                  $subject.=  $userData["events"][$id]["name"] . ", ";
 
-                $events_table .= '</td>';
-                $events_table.= "</tr>";
-
-
-                // var_dump($userData["attending"]);
+                  $events_table .= '<tr cellspacing="0" cellpadding="0">
+                    <td colspan="2" cellspacing="0" cellpadding="0" style="border-bottom: 2px solid #ECEFF1; text-align: left; padding: 20px 20px 20px 20px; font-size: 16px; color: #8B9299;">
+                      <img src="https://firebasestorage.googleapis.com/v0/b/boiling-fire-2669.appspot.com/o/close.png?alt=media&token=a2250bd0-d07a-4ffc-b10b-cf9c08566932" height="16" style="height: 12px; padding-right: 20px;"> ' . $userData["events"][$id]["name"] . '
+                    </td>
+                    </tr>';
+                  }
 
               }
 
-          }
+              if(isset($userData["playlist"][$key])) {
 
-          if($hasNotAttendingEvents) {
-            echo date('F j, Y, g:i a', ($userData["notattending"][$key]["date_created"] / 1000));
-
-            if($hasAttendingEvents) {
-              $subject .= " and not ";
-            } else {
-              $subject .= " is not attending ";
-            }
-
-            foreach($userData["notattending"][$key]["events"] as $id => $event) {
-              $subject.=  $userData["events"][$id]["name"] . ", ";
-
-              $events_table .= '<tr cellspacing="0" cellpadding="0">
-                <td colspan="2" cellspacing="0" cellpadding="0" style="border-bottom: 2px solid #ECEFF1; text-align: left; padding: 20px 20px 20px 20px; font-size: 16px; color: #8B9299;">
-                  <img src="https://firebasestorage.googleapis.com/v0/b/boiling-fire-2669.appspot.com/o/close.png?alt=media&token=a2250bd0-d07a-4ffc-b10b-cf9c08566932" height="16" style="height: 12px; padding-right: 20px;"> ' . $userData["events"][$id]["name"] . '
-                </td>
-                </tr>';
+                $notice = '<table align="center">
+                  <tr>
+                    <td style="padding-left: 20px; color: #8B9299;">
+                      <img src="' . $userData["playlist"][$key]["album_image"] . '" width="75">
+                    </td>
+                    <td style="padding-left: 20px; text-align: left;">
+                      <span style="font-size: 18px; line-height: 1.44; color: #8B9299; display: block;">' . $userData["playlist"][$key]["artist_name"] . '</span>
+                      <span style="font-size: 18px; line-height: 1.44; color: #233332; display: block; padding-bottom: 5px;">' . $userData["playlist"][$key]["track_name"] . '</span>
+                      <a href="https://play.spotify.com/track/' . $userData["playlist"][$key]["id"]. '">
+                        <img src="http://res.cloudinary.com/dtavhihxu/image/upload/v1466015803/play-with_hmyqe6.png" width="130">
+                      </a>
+                    </td>
+                  </tr>
+                </table>';
+              } else {
+                $notice = "Did not submit a track :(";
               }
 
-          }
 
-          if(isset($userData["playlist"][$key])) {
+          $events_table .= "</table>";
 
-            $notice = '<table align="center">
-              <tr>
-                <td style="padding-left: 20px; color: #8B9299;">
-                  <img src="' . $userData["playlist"][$key]["album_image"] . '" width="75">
-                </td>
-                <td style="padding-left: 20px; text-align: left;">
-                  <span style="font-size: 18px; line-height: 1.44; color: #8B9299; display: block;">' . $userData["playlist"][$key]["artist_name"] . '</span>
-                  <span style="font-size: 18px; line-height: 1.44; color: #233332; display: block; padding-bottom: 5px;">' . $userData["playlist"][$key]["track_name"] . '</span>
-                  <a href="https://play.spotify.com/track/' . $userData["playlist"][$key]["id"]. '">
-                    <img src="http://res.cloudinary.com/dtavhihxu/image/upload/v1466015803/play-with_hmyqe6.png" width="130">
-                  </a>
-                </td>
-              </tr>
-            </table>';
-          } else {
-            $notice = "Did not submit a track :(";
-          }
+          echo "<p>" . $subject . "</p>";
+
+          echo $events_table;
+          echo $notice;
 
 
-      $events_table .= "</table>";
+          $data = array(
+                    "id" => 1,
+                      "to" => $userData["email"],
+                      "attr" => array(
+                        "FNAME"=>$userData["guests"][$key]["fname"],
+                        "LNAME"=>$userData["guests"][$key]["lname"],
+                        "EVENT_LOOP"=> $events_table,
+                        "TRACK" => $notice,
+                        "DATE" => date('F j, Y, g:i a', ($guest["date_created"] / 1000)),
+                        "NOTICE" => "Amazing!",
+                        "LINK" => "http://app.cnftti.com/",
+                        "SUBJECT" => $subject
+                      )
+          );
 
-      echo "<p>" . $subject . "</p>";
+          var_dump($mailin->send_transactional_template($data));
 
-      echo $events_table;
-      // echo $notice;
+        }
 
 
-      $data = array(
-                "id" => 1,
-                  "to" => "hello@lewi.sh",
-                  "attr" => array(
-                    "FNAME"=>$userData["guests"][$key]["fname"],
-                    "LNAME"=>$userData["guests"][$key]["lname"],
-                    "EVENT_LOOP"=> $events_table,
-                    "TRACK" => $notice,
-                    "DATE" => date('F j, Y, g:i a', ($guest["date_created"] / 1000)),
-                    "NOTICE" => "Amazing!",
-                    "LINK" => "http://app.cnftti.com/",
-                    "SUBJECT" => $subject
-                  )
-      );
+      }
 
-      var_dump($mailin->send_transactional_template($data));
 
     }
-
 
   }
 
 
 }
+
+// print_r($userData);
+
 
 
 
