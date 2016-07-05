fe3cae0359805dbece503003ae518bb111179dc0
Lewis Healey <hello@lewishealey.co.uk>
2016-07-05 22:56:42 +0100

    Spotify callback listener

--- server/spotify_auth.php
+++ server/spotify_auth.php
@@ -6,7 +6,9 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$session = new SpotifyWebAPI\Session('CLIENT_ID', 'CLIENT_SECRET', 'REDIRECT_URI');
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+$session = new SpotifyWebAPI\Session('2888525482b94ccb86ae7ee9469bab07', '6df8e93ea2ba49c6ae90951fea0e2f9e', 'http://localhost:8888/confettiapp/server/spotify_access.php');
 
 $scopes = array(
     'playlist-read-private',
